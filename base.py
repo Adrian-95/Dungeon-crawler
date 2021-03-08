1e5bebf5fb1ddb0a917b50482b35e84fe60f7a53
Adrian-95 <nedlandftw@gmail.com>
2021-03-08 21:17:31 +0200

    Added Characters

--- base.py
+++ base.py
@@ -35,6 +35,41 @@ class Dice(int):
         self.value = sum(self.values[:num])
         return self
 
+class Character (object):
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.maxHealth = 100
+        attackRoll = 3*Dice(6)
+        self.attack = attackRoll.highest(2).value
+        defenseRoll = 3*Dice(6)
+        self.defense = defenseRoll.highest(2).value
+
+class Goblin (object):
+    def __init__(self):
+        healthRoll = 6*Dice(10)
+        healthValue = healthRoll.highest(5).value
+        self.health = healthValue
+        self.maxHealth = healthValue
+        if healthValue > 40:
+            self.name = "Goblin Chieftain"
+            self.description = "Strong foe"
+            self.aggression = 10
+            
+        elif healthValue > 25:
+            self.name = "Goblin Warrior"
+            self.description = "Average foe"
+            self.aggression = 8
+        else:
+            self.name = "Wild Goblin"
+            self.description = "Weak foe"
+            self.aggression = 2
+        attackRoll = 3*Dice(6)
+        self.attack = attackRoll.highest(2).value
+        defenseRoll = 3*Dice(6)
+        self.defense = defenseRoll.highest(2).value
+
+
 class Dungeon(object):
     def __init__(self):
         pass
@@ -47,3 +82,32 @@ class Room(object):
     def __init__(self):
         pass
 
+class Game (object):
+    def __init__(self):
+        self.character = None
+        self.exit = False
+
+    def run(self):
+        while not self.exit:
+            if not self.character:
+                name = input("Choose your name \n>>>")
+                name = name.strip()
+                testName = name.replace(" ", "")
+                if not testName.isalpha():
+                    print("Warning: Your name can only contain alphabetic characters.")
+                    continue
+
+                self.character = Character(name)
+
+            cmd = input("{0} HP:{1}/{2} ATT:{3} DEF:{4}>>>".format(self.character.name, self.character.health,self.character.maxHealth, self.character.attack, self.character.defense))
+
+            cmd = cmd.strip()
+
+            if cmd.lower() == "exit":
+                self.exit = True
+            else:
+                print("Warning: Command is not recognized.")
+
+            print("\n")
+
+Game().run()
